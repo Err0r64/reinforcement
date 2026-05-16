8d2b82fe7f1d9ed01b542d91ac639b13b40ec3a8
Alec Situ <92289021+Err0r64@users.noreply.github.com>
2026-05-16 14:22:55 -0700

    Compelte Q3e

--- analysis.py
+++ analysis.py
@@ -53,9 +53,9 @@ def question3d():
     # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 1
+    answerNoise = 0
+    answerLivingReward = 4
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
